488c71e3a1dd854d1e252365ede975a92a2bbd52
James Synge <jamessynge@google.com>
2022-08-23 18:15:16 -0400

    Move new http1 RequestDecoder from mcunet to mcucore. This makes sense because it doesn't actually access the network at all.

--- src/McuNet.h
+++ src/McuNet.h
@@ -6,21 +6,19 @@
 //
 // Author: james.synge@gmail.com
 
-#include "addresses.h"                        // IWYU pragma: export
-#include "connection.h"                       // IWYU pragma: export
-#include "disconnect_data.h"                  // IWYU pragma: export
-#include "eeprom_tags.h"                      // IWYU pragma: export
-#include "ethernet_address.h"                 // IWYU pragma: export
-#include "http1/request_decoder.h"            // IWYU pragma: export
-#include "http1/request_decoder_constants.h"  // IWYU pragma: export
-#include "ip_address.h"                       // IWYU pragma: export
-#include "ip_device.h"                        // IWYU pragma: export
-#include "mcunet_config.h"                    // IWYU pragma: export
-#include "platform_network.h"                 // IWYU pragma: export
-#include "platform_network_interface.h"       // IWYU pragma: export
-#include "server_socket.h"                    // IWYU pragma: export
-#include "socket_listener.h"                  // IWYU pragma: export
-#include "tcp_server_connection.h"            // IWYU pragma: export
-#include "write_buffered_connection.h"        // IWYU pragma: export
+#include "addresses.h"                   // IWYU pragma: export
+#include "connection.h"                  // IWYU pragma: export
+#include "disconnect_data.h"             // IWYU pragma: export
+#include "eeprom_tags.h"                 // IWYU pragma: export
+#include "ethernet_address.h"            // IWYU pragma: export
+#include "ip_address.h"                  // IWYU pragma: export
+#include "ip_device.h"                   // IWYU pragma: export
+#include "mcunet_config.h"               // IWYU pragma: export
+#include "platform_network.h"            // IWYU pragma: export
+#include "platform_network_interface.h"  // IWYU pragma: export
+#include "server_socket.h"               // IWYU pragma: export
+#include "socket_listener.h"             // IWYU pragma: export
+#include "tcp_server_connection.h"       // IWYU pragma: export
+#include "write_buffered_connection.h"   // IWYU pragma: export
 
 #endif  // MCUNET_SRC_MCUNET_H_
